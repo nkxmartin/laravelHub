257fa00882e84e41741b15e6015b3a0d81ff7bf3
martin <nkxmartin01@gmail.com>
2022-03-24 17:57:56 +0800

    Update code

--- checkBankAccount.php
+++ checkBankAccount.php
@@ -37,7 +37,7 @@ class checkBankAccount extends DuskTestCase
                     ->click('li.dropdown.dd-1.open li:nth-child(2) ul li:nth-child(3)')
                     ->waitFor('a#addButton',1)
                     ->type('div#filterBar-innerCt table:nth-of-type(3) td:nth-of-type(2) input','HSB12345')
-                    ->keys('div#filterBar-innerCt table:nth-of-type(3) td:nth-of-type(2) input',['{ENTER}',''],'')
+                    ->keys('div#filterBar-innerCt table:nth-of-type(3) td:nth-of-type(2) input',['{ENTER}'])
                     ->waitForTextIn('div#gridPagingToolbar-innerCt div:nth-of-type(7)','Displaying 1 - 1 of 1')
                     ->pause(2000);
         }
