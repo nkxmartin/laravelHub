877365e466fec445d8eba04c09182dd014817a2c
martin <nkxmartin01@gmail.com>
2022-04-07 12:09:43 +0800

    Update: scrap all visible column names

--- checkBankAccount.php
+++ checkBankAccount.php
@@ -3,12 +3,13 @@
 namespace Tests\Browser;
 namespace Facebook\WebDriver;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+// use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class ExampleTestEzp extends DuskTestCase
+class checkBankAccount extends DuskTestCase
 {
+    //use DatabaseMigrations;
     /**
      * A basic browser test example.
      *
@@ -30,48 +31,142 @@ class ExampleTestEzp extends DuskTestCase
         );
     }
 
-    public function checkBankAccountAdded(){
+    public function testCheckBankAccount(){
         $this->browse(function (Browser $browser){
+            $isDisplayedArrow = $browser->driver->findElement(
+                WebDriverBy::xpath("//a[contains(normalize-space(@class),'arrow')]/parent::div[@id='navbar-dropdown-showmore']"))
+                ->isDisplayed();
+
+                if (strlen(trim($isDisplayedArrow)) > 0){
+                    echo "\nArrow display?: Yes!\n";
+                    $browser->clickAtXPath("//a[contains(normalize-space(@class),'arrow')]/parent::div[@id='navbar-dropdown-showmore']");
+                }elseif (strlen(trim($isDisplayedArrow)) == 0){
+                    echo "\nArrow display?: NOTHING!\n";
+                }
+
             $browser->clickAtXPath('//a[@class="glyphicons calculator"]')
                     ->mouseover('li.dropdown.dd-1.open li.dropdown.submenu:nth-child(2)')
                     ->clickLink('Bank Accounts')
-                    ->waitFor('a#addButton',2);
+                    ->waitFor('a#addButton',2)
+                    ->assertSee('Add');
+            $this->findSearchBox();
+        });}
 
+    public function findSearchBox(){
+        $this->browse(function (Browser $browser){
+            // all of the text name of the columns
+            $columns = array("ID","Code","Alias Name","Type","Currency","Bank","Account No","Name","Usage","Address","Province",
+                            "City","Phone Number","Nation","Status","Swift Code","Sort Code","Card Id","Account Type",
+                            "Username","Email","Login URL","Remark","Created By","Modified By","Created","Modified");
+
+            // scroll to the end of the right side
+            $browser->script("
+                var myDiv = document.getElementById('gridView');
+
+                setTimeout(function() {
+                    myDiv.scrollLeft = myDiv.scrollWidth;
+                }, 500);
+            ");
+
+            $inputDynamicID = 1010;
+            $columnNameID = 1;
+
+        // 27 columns based on the array of the variable of $columns
+        for($x=0;$x<count($columns);$x++){
             // preg match to get dynamic ID in digits only from column
             $patternGetDynamicID = '/[^0-9]/';
-            
-            // get dynamic ID from the text name of the column
-            $getouterHTMLByColumnName = $browser->driver->findElement(
-                WebDriverBy::xpath('//span[contains(normalize-space(text()),"Alias Name")]'))->getAttribute('ID');
+            $columnNameID = $columnNameID + 1;
+
+            $getHTMLIDByColumnNameID[] = $browser->driver->findElement(
+                WebDriverBy::xpath(
+                    "//div[starts-with(normalize-space(@id),'headercontainer') and contains(normalize-space(@id),'innerCt')]
+                    /div[starts-with(normalize-space(@id),'headercontainer') and contains(normalize-space(@id),'targetEl')]
+                    /child::div[". $columnNameID ."]/child::div[contains(normalize-space(@id),'titleEl')]
+                    /child::span[contains(normalize-space(@id),'textEl') and not(string-length()=1)]"))
+                ->getAttribute('ID');
+            $getIDByColumnName[] = $browser->driver->findElement(
+                WebDriverBy::xpath(
+                    "//div[starts-with(normalize-space(@id),'headercontainer') and contains(normalize-space(@id),'innerCt')]
+                    /div[starts-with(normalize-space(@id),'headercontainer') and contains(normalize-space(@id),'targetEl')]
+                    /child::div[". $columnNameID ."]/child::div[contains(normalize-space(@id),'titleEl')]
+                    /child::span[contains(normalize-space(@id),'textEl') and not(string-length()=1)]"))
+                ->getText();
+
+            $getColumnNameDynamicID[] = intval(preg_replace($patternGetDynamicID,'',$getIDByColumnName[$x]));
+            $inputStaticID[] = intval($getColumnNameDynamicID[$x]) + 24;
 
             // get int from ID based on the column of the name selected
-            echo "\n" . '=========================================================================' . "\n";
-            echo 'HTML ID:' . $getouterHTMLByColumnName . "\n";
-            echo 'Column Name ID:' . $getColumnNameDynamicID = intval(preg_replace($patternGetDynamicID,'',$getouterHTMLByColumnName)) . "\n";
-            echo '=========================================================================' . "\n";
+            echo "\n=========================================================================\n";
+            if (!empty($getIDByColumnName[$x])){
+                echo "Column Name($getIDByColumnName[$x]) saved!\n";
+                $tempDisplayedColumn[] = $getIDByColumnName[$x];
+            }elseif (empty($getIDByColumnName[$x])){
+                echo "Column Name($getIDByColumnName[$x]) NOT FOUND!!!\n";
+            }
+            
+            echo "Column ID counted: ". $columnNameIDCounted = $columnNameID - 1 ."\n";
+            echo "Column Name: ".$getIDByColumnName[$x] ."\n";
+            echo "Raw Column ID: ". $getHTMLIDByColumnNameID[$x] ."\n";
+            echo "Raw Input ID + 1: ". $inputDynamicID++ ."\n";
 
+            // $inputColumnID[] = $browser->driver->findElement(WebDriverBy::xpath(
+            //     "//div[@id='filterBar']/child::div[@id='filterBar-innerCt']/child::div[@id='filterBar-targetEl']
+            //     /child::table[contains(normalize-space(@id),'triggerWrap') or starts-with(@id,'textfield') or 
+            //     starts-with(@id,'combobox') or starts-with(@id,'datetimerangefield')][".$x."]
+            //     //td[contains(normalize-space(@id),'inputCell') or contains(normalize-space(@id),'bodyEl')]
+            //     /input[contains(normalize-space(@id),'inputEl') and starts-with(@id,'textfield') or 
+            //     starts-with(@id,'combobox') or starts-with(@id,'datetimerangefield')]"))
+            //     ->getAttribute('ID');
 
-            // get dynamic ID from the search box column based on $getouterHTMLByColumnName variable
-            $columnID = intval(($getColumnNameDynamicID - 1010) + 1);
-            echo 'ID of the Search Box: ' . $columnID . "\n";
-
-            // indicate that particular Search Box based on the name of the column selected
-            $getouterHTMLBySearchBox = $browser->driver->findElement(
-                WebDriverBy::cssSelector
-                ('div#filterBar-innerCt table:nth-of-type('. $columnID .') td:nth-child(2) input'))->getAttribute('ID');
-
-            // get int from ID based on Search Box
-            echo '=========================================================================' . "\n";
-            echo 'HTML ID:' . $getouterHTMLBySearchBox . "\n";
-            echo 'Search Box ID:' . intval(preg_replace($patternGetDynamicID,'',$getouterHTMLBySearchBox)) . "\n";
-            echo '=========================================================================' . "\n";
-
-            // fill up the selected search box and do validation afterwards
-            $browser->type('div#filterBar-innerCt table:nth-of-type('. $columnID .') td:nth-child(2) input','HSB12345')
-                    ->keys('div#filterBar-innerCt table:nth-of-type('. $columnID .') td:nth-child(2) input',['{ENTER}'])
-                    ->waitForTextIn('div#gridPagingToolbar-innerCt div:nth-of-type(7)','Displaying 1 - 1 of 1')
-                    ->pause(2000);
+            // echo "Input ID based on each column: $inputColumnID[$x] \n";
+            // echo "Raw Input ID: $getColumnNameDynamicID[$x] \n";
+            // echo "Raw Input ID (calculated): $inputStaticID[$x] \n";
+            echo "=========================================================================\n";
+            }
+        for($x=0;$x<count($tempDisplayedColumn);$x++){
+            echo $tempDisplayedColumn[$x]."\n";
         }
-    );
+        echo "\n*********************************\n";
+        echo "Total visible columns found: ".count($tempDisplayedColumn)."\n";
+        echo "*********************************\n";
+    });
     }
+
+    public function displayedSearchBox(){
+        $this->browse(function (Browser $browser){
+            // all of the text name of the columns
+            $columns = array("ID","Code","Alias Name","Type","Currency","Bank","Account No","Name","Usage",
+                            "Address","Province","City","Phone Number","Nation","Status","Swift Code","Sort Code","Card Id",
+                            "Account Type","Username","Email","Login URL","Remark","Created By","Modified By","Created","Modified");
+        // 27 columns based on the array of the variable of $columns
+        for($x=0;$x<count($columns);$x++){
+            $displayedColumns[] = $browser->driver->findElement(
+                WebDriverBy::xpath(
+                    "//div[starts-with(normalize-space(@id),'headercontainer') and contains(normalize-space(@id),'innerCt')]
+                    /div[starts-with(normalize-space(@id),'headercontainer') and contains(normalize-space(@id),'targetEl')]
+                    /child::div/child::div[contains(normalize-space(@id),'titleEl')]
+                    /child::span[contains(normalize-space(@id),'textEl') 
+                    and normalize-space(.)='".$columns[$x]."']"))
+                ->isDisplayed();
+            $displayedNameColumns[] = $browser->driver->findElement(
+                WebDriverBy::xpath(
+                    "//div[starts-with(normalize-space(@id),'headercontainer') and contains(normalize-space(@id),'innerCt')]
+                    /div[starts-with(normalize-space(@id),'headercontainer') and contains(normalize-space(@id),'targetEl')]
+                    /child::div/child::div[contains(normalize-space(@id),'titleEl')]
+                    /child::span[contains(normalize-space(@id),'textEl') 
+                    and normalize-space(.)='".$columns[$x]."']"))
+                ->getText();
+
+            echo "\nNo. of count: $x\n";
+            echo "Selected Column: $columns[$x]\n";
+            echo "Column Name: $displayedNameColumns[$x]\n";
+            echo "$displayedColumns[$x]\n";
+            if (strlen(trim($displayedColumns[$x])) > 0){
+                echo "Display?: Yes!\n";
+            }elseif (strlen(trim($displayedColumns[$x])) == 0){
+                echo "Display?: NOTHING!\n";
+            }
+            
+        }
+        });}
 }
